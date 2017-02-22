2dabc8c69fae65e321a6a70580011f21cc460ac9
henrique-htg <henrique.develop@hotmail.com>
2017-02-22 14:03:45 -0300

    Corrigindo atualização do cache do client

--- service-worker.js
+++ service-worker.js
@@ -27,23 +27,23 @@ self.addEventListener('activate', function (e) {
 });
 var filesToCache = [
   '/',
-  '/index.html',
-  '/favicon.ico',
-  '/scripts/app.js',
-  '/styles/inline.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  'index.html',
+  'favicon.ico',
+  'scripts/app.js',
+  'styles/inline.css',
+  'images/clear.png',
+  'images/cloudy-scattered-showers.png',
+  'images/cloudy.png',
+  'images/fog.png',
+  'images/ic_add_white_24px.svg',
+  'images/ic_refresh_white_24px.svg',
+  'images/partly-cloudy.png',
+  'images/rain.png',
+  'images/scattered-showers.png',
+  'images/sleet.png',
+  'images/snow.png',
+  'images/thunderstorm.png',
+  'images/wind.png'
 ];
 
 self.addEventListener('fetch', function (e) {

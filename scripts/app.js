66bcbec37fc1ea3cf6b98b2efac2f18f0d273e3f
henrique-htg <henrique.develop@hotmail.com>
2017-02-22 10:46:27 -0300

    Atualizando app.js para testes com https.

--- scripts/app.js
+++ scripts/app.js
@@ -242,7 +242,7 @@
     //Registrando o arquivo service-worker
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function() {
-        navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
           // Registration was successful
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function(err) {

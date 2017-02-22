34958b83150f1da3af42d9a9725e3ed670072a25
henriquetgoncalves <henrique.develop@hotmail.com>
2017-02-22 14:02:51 -0300

    Corrigindo caminho Index.html
    Corrigindo caminho Index.html

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('activate', function (e) {
 });
 var filesToCache = [
   '/',
-  '/index.html',
+  'index.html',
   '/favicon.ico',
   '/scripts/app.js',
   '/styles/inline.css',
